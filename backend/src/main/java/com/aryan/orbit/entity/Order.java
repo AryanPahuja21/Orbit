e7e9377f39e2b1d26d402a2055322259b05fc1ce
Aryan Pahuja <aryan.pahuja@veersatech.com>
2025-07-22 22:36:22 +0530

    backend initialized

---
+++ backend/src/main/java/com/aryan/orbit/entity/Order.java
@@ -0,0 +1,4 @@
+package com.aryan.orbit.entity;
+
+public class Order {
+}
---
+++ backend/src/test/java/com/aryan/orbit/OrbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryan.orbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
