0083d8afcde0cdf1300ca055df918aacfaf2c586
Aryan Pahuja <aryanpahuja1421@gmail.com>
2025-08-03 23:42:07 +0530

    fixed docker issues

--- backend/order-service/src/main/java/com/aryan/orbit/mapper/OrderItemMapper.java
+++ backend/order-service/src/main/java/com/aryan/orbit/mapper/OrderItemMapper.java
@@ -1,29 +1,29 @@
-package com.aryan.orbit.mapper;
-
-import com.aryan.orbit.model.OrderItem;
-import com.aryan.orbit.dto.OrderItemDTO;
-
-public class OrderItemMapper {
-
-    public static OrderItemDTO toDTO(OrderItem item) {
-        if (item == null) return null;
-
-        return OrderItemDTO.builder()
-                .id(item.getId())
-                .productId(item.getProductId())
-                .quantity(item.getQuantity())
-                .price(item.getPrice())
-                .build();
-    }
-
-    public static OrderItem toEntity(OrderItemDTO dto) {
-        if (dto == null) return null;
-
-        return OrderItem.builder()
-                .id(dto.getId())
-                .productId(dto.getProductId())
-                .quantity(dto.getQuantity())
-                .price(dto.getPrice())
-                .build();
-    }
-}
+//package com.aryan.orbit.mapper;
+//
+//import com.aryan.orbit.model.OrderItem;
+//import com.aryan.orbit.dto.OrderItemDTO;
+//
+//public class OrderItemMapper {
+//
+//    public static OrderItemDTO toDTO(OrderItem item) {
+//        if (item == null) return null;
+//
+//        return OrderItemDTO.builder()
+//                .id(item.getId())
+//                .productId(item.getProductId())
+//                .quantity(item.getQuantity())
+//                .price(item.getPrice())
+//                .build();
+//    }
+//
+//    public static OrderItem toEntity(OrderItemDTO dto) {
+//        if (dto == null) return null;
+//
+//        return OrderItem.builder()
+//                .id(dto.getId())
+//                .productId(dto.getProductId())
+//                .quantity(dto.getQuantity())
+//                .price(dto.getPrice())
+//                .build();
+//    }
+//}
