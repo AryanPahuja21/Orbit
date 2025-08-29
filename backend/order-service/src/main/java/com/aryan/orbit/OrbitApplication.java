16698bf8c72f18b44484ab2ebf6d666c3f87621d
Aryan Pahuja <aryanpahuja1421@gmail.com>
2025-08-29 12:50:18 +0530

    service discovery in all services

--- backend/order-service/src/main/java/com/aryan/orbit/OrbitApplication.java
+++ backend/order-service/src/main/java/com/aryan/orbit/OrbitApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+@EnableEurekaClient
 public class OrbitApplication {
 
 	public static void main(String[] args) {
